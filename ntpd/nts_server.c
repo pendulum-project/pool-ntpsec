352ea083dfca818243c64cec6f6754af70d52aa2
Marc Schoolderman <info@squell.net>
2025-12-01 22:13:07 +0100

    add hardcoded authentication token

--- ntpd/nts_server.c
+++ ntpd/nts_server.c
@@ -48,11 +48,13 @@ static int listener6_sock = -1;
 
 #ifdef POOL_SOURCE
 static void nts_ke_pool_status_send(struct BufCtl_t *buf);
+static bool nts_ke_pool_authenticated(struct BufCtl_t *auth_token);
 #endif
 
 struct pool_query {
 	bool list;
 	struct BufCtl_t fixed_key;
+	struct BufCtl_t auth_token;
 };
 
 /* We need a lock to protect reloading our certificate.
@@ -409,11 +411,18 @@ bool nts_ke_request(SSL *ssl) {
 #else
 	struct pool_query *pool_req = NULL;
 #endif
-	/* FIXME: check the authentication status of the client and set pool_req to NULL 
-	 * if it is not an authorized pool source */
 
 	if (!nts_ke_process_receive(&buf, &aead, pool_req))
 		return false;
+
+	if (pool_req && !nts_ke_pool_authenticated(&pool_req->auth_token)) {
+		if (pool_req->list || pool_req->fixed_key.next) {
+			msyslog(LOG_DEBUG, "NTSs: Pool-ignoring attempt at inclusion in a pool");
+			/* treat the request as a unknown critical record */
+			return false;
+		}
+		pool_req = NULL;
+	}
 
 	if (pool_req && pool_req->list) {
 		buf.next = buff;
@@ -657,6 +666,16 @@ bool nts_ke_process_receive(struct BufCtl_t *buf, int *aead, struct pool_query *
 		    case nts_server_deny:
 			msyslog(LOG_DEBUG, "NTSs: ignoring nts server deny request");
 			break;
+		    case nts_auth_token:
+			if (!pool) {
+				goto unrecognized;
+			}
+			pool->auth_token.next = buf->next;
+			pool->auth_token.left = length;
+			buf->next += length;
+			buf->left -= length;
+			msyslog(LOG_DEBUG, "NTSs: received auth token");
+			break;
 		    default:
 		    unrecognized:
 			msyslog(LOG_ERR, "NTSs: received strange type: T=%d, C=%d, L=%d",
@@ -726,6 +745,35 @@ void nts_ke_pool_status_send(struct BufCtl_t *buf) {
 	ke_append_record_null(buf, NTS_CRITICAL+nts_end_of_message);
 }
 
-#endif
+/* A constant time memcmp, so that we don't like timing information
+ * related to the authentication token; the only thing that can leak through
+ * a side channel is the length of the authentication token;
+ * taken from: https://github.com/squell/dunsel/blob/master/consttime/memcmp.c
+ * with permission. */
+static int memcmp_consttime(const void *s1, const void *s2, size_t n) {
+	unsigned const char *p1 = s1, *p2 = s2;
+	int r = 0;
+	while (n--) {
+		int delta = p1[n] - p2[n];
+		r = (r & ~-delta) | delta;
+	}
+	return r;
+}
 
-/* end */
+static bool nts_ke_pool_authenticated(struct BufCtl_t *auth_token) {
+	static const char *secret[] = { "Jeff" }; /* FIXME: read from the config */
+
+	if (!auth_token->next)
+		return false;
+
+	for (size_t i=0; i < COUNTOF(secret); i++) {
+		if ((size_t)auth_token->left == strlen(secret[i]) &&
+		   (0 == memcmp_consttime(secret[i], auth_token->next, auth_token->left)))
+			return true;
+	}
+
+	msyslog(LOG_INFO, "NTSs: Pool-invalid authentication");
+	return false;
+}
+
+#endif
